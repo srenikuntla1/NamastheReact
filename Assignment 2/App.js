5b933b57667ceb78866f9a6b4b9d174eaf353e42
srenikuntla1 <srenikuntla1@student.gsu.edu>
2024-09-23 18:59:24 -0400

    complete code

---
+++ Assignment 2/App.js
@@ -0,0 +1,12 @@
+
+const heading = React.createElement('h1', {id:'title', hello: 'world'}, 'Heading 1');
+
+const heading1 = React.createElement('h2', {id:'title2'}, 'Heading 2');
+
+const container = React.createElement('div', {id:'container'}, heading, heading1);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));  
+
+root.render(container);
