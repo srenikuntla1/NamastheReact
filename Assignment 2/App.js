a6601014ac8e57467885e8132270aa88e5fd90d0
srenikuntla1 <srenikuntla1@student.gsu.edu>
2024-09-30 16:07:57 -0400

    third episode project overlay

--- Assignment 2/App.js
+++ Assignment 2/App.js
@@ -1,11 +1,69 @@
-const heading = React.createElement('h1', {id:'title', hello: 'world'}, 'Heading 1');
+/**
+ * Created a Server
+ * Hot Module Reload
+ * File Watcher algorithm - c++ 
+ * minify
+ * bundling
+ * cleaning our code
+ * dev and production build
+ * super fast build algorithm
+ * image optimization
+ * Caching while development
+ * compression
+ * Compatible with older version of browser
+ * https on dev
+ * port number
+ * Consistent hashing algorithm
+ * zero configuration
+ * Tree shaking - removing unwanted code
+ * 
+ * TRANSITIVE DEPENDENCIES -> dependencies of dependencies
+ * 
+ * 
+ * Polyfill - if browser doesn't understand the code, it will convert it to the code that browser understands
+ * babel - convert the code to the code that browser understands
+ */
 
-const heading1 = React.createElement('h2', {id:'title2'}, 'Heading 2');
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const container = React.createElement('div', {id:'container'}, heading, heading1);
+const heading = React.createElement(
+    'h1', 
+    {
+        id:'title', 
+        hello: 'world', 
+        key:"h1"
+    }, 
+    'Heading 1 from parcel'
+);
 
-console.log(heading);
+const data = "hi";
+
+
+//React element
+const Title = ()=>(<h1>
+    Namaste React
+    </h1>);
+
+const heading1 = React.createElement('h2', {id:'title2', key:"h2"}, 'Heading 2');
+
+// Functional component is a normal function
+//JSX does sanity check
+//component composition - using component inside another component
+const HeaderComponent = ()=> {
+
+    return (
+       <div>
+        {data}
+        <Title/>
+        <h1>Namaste React Functional Component</h1>
+        <h2>This is a H2 tag</h2>
+       </div>
+    );
+
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));  
 
-root.render(container);
+root.render(<HeaderComponent/>);
